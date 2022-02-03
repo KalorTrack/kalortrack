98859b0acf872319111aa99998fae3d11ab8bd32
Carolina Lopes <carolina.b.lopes@hotmail.com>
2022-02-02 22:00:53 -0300

    Update KalorTrack.java

--- KalorTrack.java
+++ KalorTrack.java
@@ -4,28 +4,20 @@ import java.util.Scanner;
 
 public class KalorTrack {
 
-	public static void main(String[] args) {
-		
-		Scanner leia = new Scanner(System.in);
-		
-		cadastro cad = new cadastro(null, null, 0, 0, 0, 0);
-		
-		int ligado = 1;
-		
-		do {
-		
-		cad.inicioCadastro();
-		
-		cad.appSelect();
-	
-		
-		} while(ligado == 1);
-	}
+    public static void main(String[] args) {
+
+        Scanner leia = new Scanner(System.in);
+
+        cadastro cad = new cadastro(null, null, 0, 0, 0, 0);
+
+        int ligado = 1;
+
+        do {
+
+            cad.inicioCadastro();
+
+            cad.appSelect();
+
+        } while (ligado == 1);
+    }
 }
-
-	
-		
-
-
-
-
